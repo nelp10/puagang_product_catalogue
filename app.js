956d2c7dfc228f21d69958dbfa6485fcede9b7be
nelp10 <20221805@nbsc.edu.ph>
2024-02-15 16:17:48 +0800

    added cancel button

--- app.js
+++ app.js
@@ -7,29 +7,32 @@ fetch('data.json')
             const cardItem = document.createElement('div');
             cardItem.classList.add('card');
 
-            let clickCount = 0; 
+            let clickCount = 0;
 
             cardItem.innerHTML = `
-                <div class="img-box">
-                    <alt="${product.Name}">
-                </div>
                 <div class="card" style="width: 18rem;">
-  <alt="...">
-  <div class="details">
-    <h5>${product.Name}</h5>
-    <p class="card-text">${product.price}</p>
-    <button class="addToCartBtn">Add to Cart</button>
-    <p>Quantity: <span class="clickCount">${clickCount}</span></p>
-    
-  </div>
-</div>
+                    <alt="#">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.Name}</h5>
+                            <p class="card-text">${product.price}</p>
+                            <button class="addToCartBtn">Add to Cart</button>
+                            <button class="cancelBtn">Cancel</button>
+                            <p> Quantity: <span class="clickCount">${clickCount}</span></p>
+                        </div>
+                </div>
             `;
 
             const addToCartButton = cardItem.querySelector('.addToCartBtn');
+            const cancelBtn = cardItem.querySelector('.cancelBtn');
             const clickCountElement = cardItem.querySelector('.clickCount');
 
             addToCartButton.addEventListener('click', () => {
                 clickCount++;
+                clickCountElement.textContent = clickCount;
+            });
+
+            cancelBtn.addEventListener('click', () => {
+                clickCount = 0;
                 clickCountElement.textContent = clickCount;
             });
 
